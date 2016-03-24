09ead6e624e74a50a811ba31d60866ad9b3f061e
fluid10 <ugardun@gmail.com>
2016-03-24 16:40:36 +0000

    Changed Heroku URL

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -151,8 +151,8 @@ class ApplicationController < ActionController::Base
               start_month = params[:start_months][index]
               start_year = params[:start_years][index]
               changed = true
-            else
-              changed = false
+            # else
+            #   changed = false
             end     
           else
             changed = false

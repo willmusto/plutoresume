5429bc38d1c4f38a44267194b0eb836429d6e5e1
fluid10 <ugardun@gmail.com>
2016-03-24 18:15:15 +0000

    Changed Heroku URL

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -30,7 +30,6 @@ class ResumesController < ApplicationController
 
 	# Uploads authenticated user's resume to GDrive's master shared folder
 	def create
-		# fail 
 		unless session.has_key?(:credentials)
 			return redirect_to('/auth/google_oauth2/callback')
 		end
